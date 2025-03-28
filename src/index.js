2d88cc0abc3e39c6bbf0aaeaf265831cd43d6cd8
sasa24 <hoge@hoge>
2025-03-28 14:18:53 +0900

    refactor: 一旦コミット

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import { TetrisApp } from './components/TetrisApp.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Bootstrapのツールチップを初期化
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
-
 // アプリケーションのインスタンスを作成
 new TetrisApp(); 
